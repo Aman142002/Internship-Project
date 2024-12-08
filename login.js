e64c8f596c0c4bec88eec00ee68f6440ec90ff37
Aman Kumar <amandeep111421@gmail.com>
2024-12-08 22:46:19 +0530

    login.js

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
 
     try {
         // Send the login request to the backend API
-        const response = await fetch('http://localhost:3000/api/v1/users/login', {
+        const response = await fetch('https://internship-project-8ub3.onrender.com/api/v1/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
